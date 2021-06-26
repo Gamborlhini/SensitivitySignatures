44dec87d60e7b6e96ae7779483936cc693f27439
Gamborlhini <nikhil.subhas@gmail.com>
2021-06-26 15:56:45 -0400

    Limma DE added (not working)
    DE not working, can't figure out which orientation the dataframe is supposed to be in

--- SensitivitySigs.R
+++ SensitivitySigs.R
@@ -7,10 +7,12 @@ expressionData0 <- data.frame(read.csv(file = "out_0.csv", check.names = FALSE))
 expressionData1 <- data.frame(read.csv(file = "out_1.csv", check.names = FALSE))
 expressionData2 <- data.frame(read.csv(file = "out_2.csv", check.names = FALSE))
 expressionData3 <- data.frame(read.csv(file = "out_3.csv", check.names = FALSE))
+#combine all the expression data into one frame
 combinedEXP <- rbind(expressionData0, expressionData1, expressionData2, expressionData3)
 
+#turn the raw IC50 data into a df
 frame <- data.frame(rawData)
-print(names(combinedEXP))
+
 
 #filter frame to only include cisplatin
 cis <- frame[frame$DRUG_NAME == "Cisplatin", c("CELL_LINE_NAME", "COSMIC_ID", "TCGA_DESC", "DRUG_NAME","LN_IC50")]
@@ -23,9 +25,6 @@ cisSKCM <- cis[cis$TCGA_DESC == "SKCM", c("CELL_LINE_NAME", "COSMIC_ID", "TCGA_D
 #reassemble the dataframe
 filteredCis <- rbind(cisTHCA, cisLIHC)
 filteredCis <- rbind(filteredCis, cisSKCM)
-
-#display the number of cell lines in the filtered frame
-print(nrow(filteredCis))
 
 #takes a dataframe and returns the top 20% based on the last column values (LN_IC50)
 getTop <- function(x){
@@ -40,13 +39,35 @@ getBottom <- function(x){
 #Creates dataframes for the top and bottom responders
 top <- getTop(filteredCis)
 bottom <- getBottom(filteredCis)
-#set up boolean for sensitivity
+#set up boolean explanatory variable for sensitivity
 top['sensitivity'] <- TRUE
 bottom['sensitivity'] <- FALSE
 
+#recombines the top and bottom 20%
 allSens <- rbind(top, bottom)
+#selects only the things we need for limma
+sensForJoin <- allSens[, c("COSMIC_ID", "sensitivity")]
 #Do the inner join
-matchedSens <- inner_join(allSens, combinedEXP, by = c("COSMIC_ID" = "GENE_SYMBOLS"))
+matchedSens <- inner_join(combinedEXP, sensForJoin, by = c("GENE_SYMBOLS" = "COSMIC_ID"))
 print(allSens)
 
-print(matchedSens)
+
+#limma stuff from here on out
+#make the design matrix
+design <- model.matrix(~sensitivity, data = matchedSens)
+
+#check to see that it looks right
+print(head(design, 2))
+#check the number of samples against the number of samples in the original set
+colSums(design)
+table(matchedSens[, "sensitivity"])
+
+#use limma to do DE
+fit <- lmFit(matchedSens, design)
+fit <- eBayes(fit)
+
+#This code is to rotate the dataset later if I need
+
+#transposed <- as.data.frame(t(matchedSens[,-1]))
+#colnames(transposed) <- rownames(matchedSens)
+#rownames(transposed) <- colnames(matchedSens)
